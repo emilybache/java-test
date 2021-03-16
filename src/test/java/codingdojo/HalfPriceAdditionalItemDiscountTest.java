6a765ec439fad8397207ff4fdd903f083ac0378b
emily <emily@bacheconsulting.com>
2021-03-16 17:03:52 +0100

    no discounts apply outside valid date range
    implementation still faked in HalfPriceAdditionalDiscount

--- src/test/java/codingdojo/HalfPriceAdditionalItemDiscountTest.java
+++ src/test/java/codingdojo/HalfPriceAdditionalItemDiscountTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,11 +12,19 @@ public class HalfPriceAdditionalItemDiscountTest {
 
     public static final LocalDate TODAY = LocalDate.now();
     private DatedBasket basket = new DatedBasket();
+    Stock stock = new Stock(List.of(new StockItem("soup", 65), new StockItem("bread", 80)));
+
 
     @Test
     public void noDiscountOnEmptyBasket() {
-        Stock stock = new Stock(Collections.emptyList());
         Discount d = new HalfPriceAdditionalDiscount(stock, TODAY.minusDays(1), TODAY.plusDays(7), "soup", "bread");
+        assertEquals(0, d.apply(basket));
+    }
+
+    @Test
+    public void discountDoesntApplyOutsideDateRange() {
+        HalfPriceAdditionalDiscount d = new HalfPriceAdditionalDiscount(stock, TODAY.plusDays(1), TODAY.plusDays(7), "soup", "bread");
+        basket.purchaseDate = 0;
         assertEquals(0, d.apply(basket));
     }
 }
