c79059b00ef8b415affbf7253dd0598c49b9436f
emily <emily@bacheconsulting.com>
2021-03-16 16:33:44 +0100

    refactor test for readability

--- src/test/java/codingdojo/TenPercentDiscountTest.java
+++ src/test/java/codingdojo/TenPercentDiscountTest.java
@@ -1,5 +1,6 @@
 package codingdojo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -11,20 +12,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TenPercentDiscountTest {
 
     public static final LocalDate TODAY = LocalDate.now();
+    private DatedBasket basket = new DatedBasket();
 
 
     @Test
     public void noDiscountWhenWrongKindOfItem() {
         Stock stock = new Stock(Collections.emptyList());
         Discount d = new TenPercentDiscount("apples", stock, TODAY.minusDays(1), TODAY.plusDays(7));
-        assertEquals(0, d.apply(new DatedBasket()));
+        assertEquals(0, d.apply(basket));
     }
 
     @Test
     public void discountAppliesToAllItems() {
         Stock stock = new Stock(List.of(new StockItem("apples", 10)));
         Discount d = new TenPercentDiscount("apples", stock, TODAY.minusDays(1), TODAY.plusDays(7));
-        DatedBasket basket = new DatedBasket();
         basket.basket.add(new ItemQuantity("apples", 5));
         assertEquals(5, d.apply(basket));
     }
@@ -33,7 +34,6 @@ public class TenPercentDiscountTest {
     public void discountIsTenPercent() {
         Stock stock = new Stock(List.of(new StockItem("milk", 130)));
         Discount d = new TenPercentDiscount("milk", stock, TODAY.minusDays(1), TODAY.plusDays(7));
-        DatedBasket basket = new DatedBasket();
         basket.basket.add(new ItemQuantity("milk", 1));
         assertEquals(13, d.apply(basket));
     }
