69df57c1e248cb435e2937cd06b421005fcf504b
emily <emily@bacheconsulting.com>
2021-03-17 08:10:31 +0100

    generalize so HalfPriceAdditionalItemDiscount works in most cases now

--- src/main/java/codingdojo/HalfPriceAdditionalItemDiscount.java
+++ src/main/java/codingdojo/HalfPriceAdditionalItemDiscount.java
@@ -3,11 +3,11 @@ package codingdojo;
 import java.time.LocalDate;
 import java.util.List;
 
-public class HalfPriceAdditionalDiscount extends Discount {
+public class HalfPriceAdditionalItemDiscount extends Discount {
     private String mainItem;
     private String additionalItem;
 
-    public HalfPriceAdditionalDiscount(Stock stock, LocalDate validFrom, LocalDate validUntil, String mainItem, String additionalItem) {
+    public HalfPriceAdditionalItemDiscount(Stock stock, LocalDate validFrom, LocalDate validUntil, String mainItem, String additionalItem) {
         super(stock, validFrom, validUntil);
 
         this.mainItem = mainItem;
@@ -20,11 +20,9 @@ public class HalfPriceAdditionalDiscount extends Discount {
             return 0;
         }
         long discount = 0;
-        if (basketQuantity(datedBasket.basket, this.mainItem) >= 2
-                && basketQuantity(datedBasket.basket, additionalItem) >= 1) {
-
-            discount += stock.priceFor(additionalItem)*0.5;
-        }
+        long numberOfTimesPossibleToApplyDiscount = basketQuantity(datedBasket.basket, this.mainItem) / 2;
+        long actualNumberOfTimesToApplyDiscount = Math.min(numberOfTimesPossibleToApplyDiscount, basketQuantity(datedBasket.basket, additionalItem));
+        discount = Math.round(stock.priceFor(additionalItem)*0.5*actualNumberOfTimesToApplyDiscount);
 
         return discount;
     }
--- src/test/java/codingdojo/HalfPriceAdditionalItemDiscountTest.java
+++ src/test/java/codingdojo/HalfPriceAdditionalItemDiscountTest.java
@@ -3,7 +3,6 @@ package codingdojo;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,33 +12,45 @@ public class HalfPriceAdditionalItemDiscountTest {
     public static final LocalDate TODAY = LocalDate.now();
     private DatedBasket basket = new DatedBasket();
     Stock stock = new Stock(List.of(new StockItem("soup", 65), new StockItem("bread", 80)));
+    private HalfPriceAdditionalItemDiscount discount = new HalfPriceAdditionalItemDiscount(stock, TODAY.minusDays(1), TODAY.plusDays(7), "soup", "bread");;
 
 
     @Test
     public void noDiscountOnEmptyBasket() {
-        Discount d = new HalfPriceAdditionalDiscount(stock, TODAY.minusDays(1), TODAY.plusDays(7), "soup", "bread");
-        assertEquals(0, d.apply(basket));
+        assertEquals(0, discount.apply(basket));
     }
 
     @Test
     public void discountDoesntApplyOutsideDateRange() {
-        HalfPriceAdditionalDiscount d = new HalfPriceAdditionalDiscount(stock, TODAY.plusDays(1), TODAY.plusDays(7), "soup", "bread");
         basket.purchaseDate = 0;
-        assertEquals(0, d.apply(basket));
+        discount.updateValidPeriod(TODAY.plusDays(1), TODAY.plusDays(7));
+        assertEquals(0, discount.apply(basket));
     }
 
     @Test
     public void discountAppliesWithTwoMainAndOneAdditionalItem() {
-        HalfPriceAdditionalDiscount d = new HalfPriceAdditionalDiscount(stock, TODAY.minusDays(1), TODAY.plusDays(7), "soup", "bread");
         basket.basket.add(new ItemQuantity("soup", 2));
         basket.basket.add(new ItemQuantity("bread", 1));
-        assertEquals(40, d.apply(basket));
+        assertEquals(40, discount.apply(basket));
     }
 
     @Test
-    public void shouldntCrashIfNoAdditionalItem() {
-        HalfPriceAdditionalDiscount d = new HalfPriceAdditionalDiscount(stock, TODAY.minusDays(1), TODAY.plusDays(7), "soup", "bread");
+    public void shouldntDiscountIfNoAdditionalItem() {
         basket.basket.add(new ItemQuantity("soup", 2));
-        assertEquals(0, d.apply(basket));
+        assertEquals(0, discount.apply(basket));
+    }
+
+    @Test
+    public void applyDiscountTwiceWhenManyMainItemsAndAdditionalItems() {
+        basket.basket.add(new ItemQuantity("soup", 4));
+        basket.basket.add(new ItemQuantity("bread", 4));
+        assertEquals(80, discount.apply(basket));
+    }
+
+    @Test
+    public void doesntGiveYouMoreDiscountedItemsThanYouBought() {
+        basket.basket.add(new ItemQuantity("soup", 4));
+        basket.basket.add(new ItemQuantity("bread", 1));
+        assertEquals(40, discount.apply(basket));
     }
 }
